d99e65381fe39404651b9e44e76c50a6531cb6ea
AbdulAhad2659 <102375793+AbdulAhad2659@users.noreply.github.com>
2022-12-21 21:34:46 +0500

    Implemented the code using recursion

---
+++ Towers of Hanoi console application.py
@@ -0,0 +1,23 @@
+def tower_of_hanoi(n, source, auxiliary, target):
+    if n == 1:
+        print("Move disk 1 from source", source, "to target", target)
+
+        return
+
+    tower_of_hanoi(n - 1, source, target, auxiliary)
+
+    print("Move disk", n, "from source", source, "to target", target)
+
+    tower_of_hanoi(n - 1, auxiliary, source, target)
+
+
+# Get the number of disks from the user
+num_disks = int(input("Enter the number of disks: "))
+print("\n********************************************************************************************************************************************************************")
+print("Disk 1 is the smallest and Disk", num_disks, "is the largest. The aim is to shift all the disks from source "
+                                                      "to the target column by only placing a smaller disk on a larger"
+                                                      " disk")
+print("********************************************************************************************************************************************************************\n")
+
+# Solve the Tower of Hanoi game with the given number of disks
+tower_of_hanoi(num_disks, "Column 1", "Column 2", "Column 3")
